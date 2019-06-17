ea2ea12533f97167da5c6cd663f7556525ecb120
Administrator <Administrator@WIN-OU1OPV170OJ>
2019-06-17 10:21:25 -0700

    Test Commit

--- app/src/main/java/com/example/bananacultivation/MainActivity.java
+++ app/src/main/java/com/example/bananacultivation/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.bananacultivation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.database.AccountDB;
+import com.example.entities.Account;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,7 +47,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonLogin_onClick(View view){
+        AccountDB accountDB=new AccountDB(getApplicationContext());
+        String username=editTextusername.getText().toString();
+        String password=editTextPassword.getText().toString();
+        Account account=accountDB.login(username,password);
+        if (account==null){
+            AlertDialog.Builder builder=new AlertDialog.Builder(view.getContext());
+            builder.setTitle(R.string.error);
+            builder.setMessage(R.string.invalid_account);
+            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            builder.show();
+        }
+        else{
+            Intent intent=new Intent(MainActivity.this,WelcomeActivity.class);
+            intent.putExtra("account",account);
+            startActivity(intent);
 
+        }
     }
 
     public void buttonSignUp_onClick(View view){
--- app/src/main/java/com/example/bananacultivation/SignUpActivity.java
+++ app/src/main/java/com/example/bananacultivation/SignUpActivity.java
@@ -2,11 +2,16 @@ package com.example.bananacultivation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.database.AccountDB;
+import com.example.entities.Account;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -27,6 +32,13 @@ public class SignUpActivity extends AppCompatActivity {
         buttonSave=findViewById(R.id.buttonSave);
         buttonCancel=findViewById(R.id.buttonCancel);
 
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonSave_onClick(view);
+            }
+        });
+
         buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,5 +46,61 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+    }
+    public void buttonSave_onClick(View view) {
+        try{
+            AccountDB accountDB=new AccountDB(getApplicationContext());
+            Account account=new Account();
+            account.setEmail(editTextEmail.getText().toString());
+            account.setFullName(editTextFullName.getText().toString());
+            account.setPassword(editTextPassword.getText().toString());
+            account.setUsername(editTextusername.getText().toString());
+            Account temp=accountDB.checkUsername(editTextusername.getText().toString());
+            if (temp == null){
+                if (accountDB.create(account)){
+                Intent intent=new Intent(SignUpActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+            else {
+                AlertDialog.Builder builder=new AlertDialog.Builder(view.getContext());
+                builder.setTitle(R.string.error);
+                builder.setMessage(R.string.can_not_create);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                builder.show();
+            }
+        }
+            else {
+                AlertDialog.Builder builder=new AlertDialog.Builder(view.getContext());
+                builder.setTitle(R.string.error);
+                builder.setMessage(R.string.username_exists);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                builder.show();
+            }
+        }
+        catch (Exception e){
+            AlertDialog.Builder builder=new AlertDialog.Builder(view.getContext());
+            builder.setTitle(R.string.error);
+            builder.setMessage(e.getMessage());
+            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            builder.show();
+        }
+
     }
 }
